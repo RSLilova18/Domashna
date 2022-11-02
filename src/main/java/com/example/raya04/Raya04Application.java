75b8348f0ab03753591b2b64b6a8170cbaa478bd
RSLilova18 <RSLilova18@codingburgas.bg>
2022-11-02 14:26:39 +0200

    Hello World (test)

---
+++ src/main/java/com/example/raya04/Raya04Application.java
@@ -0,0 +1,12 @@
+package com.example.raya04;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Raya04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Raya04Application.class, args);
+    }
+
+}
