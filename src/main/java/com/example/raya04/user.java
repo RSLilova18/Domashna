12581491c349c7eff58b29bb5b97e807f254a2f2
RSLilova18 <RSLilova18@codingburgas.bg>
2022-11-02 14:29:45 +0200

    user info (/user)

---
+++ src/main/java/com/example/raya04/user.java
@@ -0,0 +1,14 @@
+package com.example.raya04;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class user {
+
+    @GetMapping("/user")
+    public String info(){
+        return "Raya Lilova";
+    }
+}
